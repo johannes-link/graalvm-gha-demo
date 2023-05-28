69b8ce6d70f91363033227298f3ce5ee6ee58a24
Johannes Link <link.johannes@pm.me>
2023-05-28 20:52:03 +0200

    Initial commit

---
+++ src/main/java/com/link/graalvmghademo/GraalvmGhaDemoApplication.java
@@ -0,0 +1,13 @@
+package com.link.graalvmghademo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GraalvmGhaDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GraalvmGhaDemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/link/graalvmghademo/GraalvmGhaDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.link.graalvmghademo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GraalvmGhaDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
